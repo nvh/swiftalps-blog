e5a1ae8ea3ad93700af6afcae66246ceed2748b4
Niels van Hoorn <nvh@nvh.io>
2016-11-10 17:39:50 +0100

    Serving static content

--- server/Sources/main.swift
+++ server/Sources/main.swift
@@ -1,13 +1,23 @@
 import Kitura
+import HeliumLogger
+import Foundation
 
 // Create a new router
 let router = Router()
 
+// Initialize HeliumLogger
+HeliumLogger.use()
+
+let fileManager = FileManager.default
+
+let staticServer = StaticFileServer(path: "../web/")
+
+print(fileManager.currentDirectoryPath)
+
 // Handle HTTP GET requests to /
 router.get("/") {
     request, response, next in
-    response.send("Hello, World!")
-    next()
+        staticServer.handle(request: request, response: response, next: next)
 }
 
 
